a8c1b7e32af100f49fb5f97a77676adcf8268d07
Parker Thompson <park.thompson@gmail.com>
2021-04-30 18:55:27 -0500

    Update logthis.js

--- logthis.js
+++ logthis.js
@@ -2,6 +2,12 @@ function logThis() {
     console.log(document.getElementById("input").value);
 }
 
+function makeParkerSay() {
+    console.log(`Parker says ${document.getElementById("input").value}`);
+}
+
 document.getElementById("button").addEventListener("click", logThis);
+document.getElementById("button-parker").addEventListener("click", makeParkerSay);
 
 //This is a lot of fun
+
