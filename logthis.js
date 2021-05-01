719627d38c1db3cd5ccef2ff033c808786966c1b
Parker Thompson <park.thompson@gmail.com>
2021-04-30 19:09:05 -0500

    Update logthis.js

--- logthis.js
+++ logthis.js
@@ -1,13 +1,22 @@
 function logThis() {
     console.log(document.getElementById("input").value);
+    buttonIncrease(buttonCount);
 }
 
 function makeParkerSay() {
     console.log(`Parker says ${document.getElementById("input").value}`);
+    buttonIncrease(buttonCount);
 }
 
 document.getElementById("button").addEventListener("click", logThis);
 document.getElementById("button-parker").addEventListener("click", makeParkerSay);
+
+let buttonCount = 0;
+
+function buttonIncrease(currentCount) {
+    buttonCount = currentCount + 1;
+    document.getElementById("button-text").innerHTML = `You've pressed my buttons ${buttonCount} times. Refresh to reset!`;
+}
 
 //This is a lot of fun
 
